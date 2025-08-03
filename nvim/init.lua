60c021de1d61e405e52691811458bf989a216104
William Beinö <william.beino@gamifiera.com>
2025-08-03 23:04:44 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,14 @@
+local vim = _G['vim'] -- useless line to remove unnecessary warnings
+
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.swapfile = false
-vim.g.mapleader = " "
-vim.o.winborder = "rounded"
-vim.o.clipboard = "unnamedplus"
+vim.g.mapleader = ' '
+vim.o.winborder = 'rounded'
+vim.o.clipboard = 'unnamedplus'
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
@@ -16,22 +18,19 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>t', ':e /tmp/todo.md<CR>')
 
 vim.pack.add({
-	{ src = "https://github.com/tpope/vim-sleuth" },         -- tabs and spaces idk
+	{ src = 'https://github.com/tpope/vim-sleuth' },             -- tabs and spaces idk
 
-	{ src = "https://github.com/Mofiqul/vscode.nvim" },      -- theme
+	{ src = 'https://github.com/Mofiqul/vscode.nvim' },          -- theme
 
-	{ src = "https://github.com/stevearc/oil.nvim" },        -- best file explorer
+	{ src = 'https://github.com/stevearc/oil.nvim' },            -- best file explorer
 
-	{ src = "https://github.com/mason-org/mason.nvim" },     -- easier installation of lsp servers
-	{ src = "https://github.com/neovim/nvim-lspconfig" },    -- lsp
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }, -- treesitter good
+	{ src = 'https://github.com/mason-org/mason.nvim' },         -- easier installation of lsp servers
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },        -- lsp
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' }, -- treesitter good
 
-	{ src = "https://github.com/echasnovski/mini.ai" },      -- better around and inner
-	{ src = "https://github.com/echasnovski/mini.pick" },    -- simple search files
-
-	-- { src = "https://github.com/echasnovski/mini.completion" }, -- completion
-	-- { src = "https://github.com/echasnovski/mini.snippets" }, -- snippets
-	-- { src = "https://github.com/rafamadriz/friendly-snippets" }, -- cool snippets database
+	{ src = 'https://github.com/echasnovski/mini.ai' },          -- more text-objects
+	{ src = 'https://github.com/echasnovski/mini.pick' },        -- hmm still evaluating this one
+	{ src = 'https://github.com/xTacobaco/llmao.nvim' }	     -- sick llm tool idk, just testing
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -42,37 +41,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		end
 	end,
 })
-vim.cmd("set completeopt+=noselect")
+vim.cmd('set completeopt+=noselect')
 
-require "nvim-treesitter.configs".setup({
-	ensure_installed = { "typescript", "javascript" },
+require('nvim-treesitter.configs').setup({
+	ensure_installed = { 'typescript', 'javascript' },
 	highlight = { enable = true }
 })
-require "oil".setup()
-require "mason".setup()
+require('oil').setup()
+require('mason').setup()
 
-require 'mini.ai'.setup()
-require "mini.pick".setup()
--- require "mini.completion".setup()
--- require "mini.snippets".setup()
+require('mini.ai').setup()
+require('mini.pick').setup()
 
-vim.keymap.set('n', '<leader>sf', ":Pick files<CR>")
-vim.keymap.set('n', '<leader>sb', ":Pick buffers<CR>")
-vim.keymap.set('n', '<leader>sg', ":Pick grep_live<CR>")
+require('llmao.nvim').setup({ api_key = os.getenv "CLAUDE_API_KEY" })
 
-vim.keymap.set('n', '-', ":Oil<CR>")
+vim.keymap.set('n', '<leader>sf', ':Pick files<CR>')
+vim.keymap.set('n', '<leader>sb', ':Pick buffers<CR>')
+vim.keymap.set('n', '<leader>sg', ':Pick grep_live<CR>')
+
+vim.keymap.set('n', '-', ':Oil<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.lsp.enable({ "lua_ls", "ts_ls" })
+vim.lsp.enable({ 'lua_ls', 'ts_ls' })
 
-vim.o.background = 'dark'
-local c = require('vscode.colors').get_colors()
 require('vscode').setup({
 	transparent = true,
 	italic_comments = true,
 	underline_links = true,
-	disable_nvimtree_bg = true,
-	terminal_colors = true,
 })
-
-vim.cmd.colorscheme "vscode"
-vim.cmd(":hi statusline guibg=NONE")
+vim.cmd('colorscheme vscode')
+vim.cmd(':hi statusline guibg=NONE')
