b8281219cc344113b6cfdbc6246da2d369dd8f3d
william.beino <william.beino@gamifiera.com>
2025-08-04 14:50:28 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,26 +11,32 @@ vim.o.winborder = 'rounded'
 vim.o.clipboard = 'unnamedplus'
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.splitright = true
 
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>t', ':e /tmp/todo.md<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.pack.add({
-	{ src = 'https://github.com/tpope/vim-sleuth' },             -- tabs and spaces idk
+	{ src = 'https://github.com/tpope/vim-sleuth' },				-- tabs and spaces idk
 
-	{ src = 'https://github.com/Mofiqul/vscode.nvim' },          -- theme
+	{ src = 'https://github.com/Mofiqul/vscode.nvim' },				-- theme
 
-	{ src = 'https://github.com/stevearc/oil.nvim' },            -- best file explorer
+	{ src = 'https://github.com/stevearc/oil.nvim' },				-- best file explorer
 
-	{ src = 'https://github.com/mason-org/mason.nvim' },         -- easier installation of lsp servers
-	{ src = 'https://github.com/neovim/nvim-lspconfig' },        -- lsp
+	{ src = 'https://github.com/mason-org/mason.nvim' },			-- lsp installer
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },  -- easier installation of lsp servers
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },			-- lsp configs
+
 	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' }, -- treesitter good
 
-	{ src = 'https://github.com/echasnovski/mini.ai' },          -- more text-objects
-	{ src = 'https://github.com/echasnovski/mini.pick' },        -- hmm still evaluating this one
-	{ src = 'https://github.com/xTacobaco/llmao.nvim' }	     -- sick llm tool idk, just testing
+	{ src = 'https://github.com/echasnovski/mini.ai' },				-- good text-objects
+	{ src = 'https://github.com/echasnovski/mini.surround' },		-- good surround 
+
+	{ src = 'https://github.com/nvim-lua/plenary.nvim' },			-- needed for telescope 
+	{ src = 'https://github.com/nvim-telescope/telescope.nvim' }	-- best file picker
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -49,19 +55,18 @@ require('nvim-treesitter.configs').setup({
 })
 require('oil').setup()
 require('mason').setup()
+require("mason-lspconfig").setup({ automatic_enable = false })
 
 require('mini.ai').setup()
-require('mini.pick').setup()
+require('mini.surround').setup()
 
-require('llmao.nvim').setup({ api_key = os.getenv "CLAUDE_API_KEY" })
-
-vim.keymap.set('n', '<leader>sf', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>sb', ':Pick buffers<CR>')
-vim.keymap.set('n', '<leader>sg', ':Pick grep_live<CR>')
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope buffers' })
 
 vim.keymap.set('n', '-', ':Oil<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
-vim.lsp.enable({ 'lua_ls', 'ts_ls' })
 
 require('vscode').setup({
 	transparent = true,
