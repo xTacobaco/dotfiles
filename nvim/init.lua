9eea7a4dbf1abcfd6106380ad3247e0b00797b27
William Beinö <william.beino@gamifiera.com>
2025-08-08 17:54:56 +0200

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -192,4 +192,14 @@ require("lazy").setup({
       { "<leader>sb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
     },
   },
+
+  -- Best plugin
+  {
+    "xTacobaco/cursor-agent.nvim",
+    config = function()
+      vim.keymap.set("n", "<leader>ca", ":CursorAgent<CR>", { desc = "Cursor Agent: Toggle terminal" })
+      vim.keymap.set("v", "<leader>ca", ":CursorAgentSelection<CR>", { desc = "Cursor Agent: Send selection" })
+      vim.keymap.set("n", "<leader>cA", ":CursorAgentBuffer<CR>", { desc = "Cursor Agent: Send buffer" })
+    end,
+  }
 })
